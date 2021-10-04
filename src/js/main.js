db55fd3352ad93954804ef54aed766e7da4787c0
Dmitry <lupanov5@mail.ru>
2021-10-04 17:48:21 +0300

    Implemented bottom slider

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,8 @@
 import modals from "./modules/modals"
+import sliders from "./modules/sliders"
 
 window.addEventListener('DOMContentLoaded', () => {
     'use strict'
     modals()
+    sliders('.feedback-slider-item', '', '.main-prev-btn', '.main-next-btn')
 })
